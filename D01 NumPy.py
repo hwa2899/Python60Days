a70b5b7d757ea11659bf222c6230af03bda63722
hwa2899 <77541111+hwa2899@users.noreply.github.com>
2021-01-21 14:10:38 +0800

    Add files via upload

---
+++ D01 NumPy.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 21 12:25:53 2021
+
+@author: acer
+"""
+import numpy as np
+#1.生成一個等差數列，首數為 0，尾數為 20，公差為 1 的數列。
+a = np.arange(0,21,1)
+print(a)
+#2.呈上題，將以上數列取出偶數。
+even = a[a%2==0]
+print(even)
+
+#3.呈 1 題，將數列取出 3 的倍數。
+triple = a[a%3==0]
+print(triple)
