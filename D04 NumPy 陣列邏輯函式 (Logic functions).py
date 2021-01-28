73f3640ca921e1b441d2754d9f9bad2227749a5d
hwa2899 <77541111+hwa2899@users.noreply.github.com>
2021-01-28 15:45:12 +0800

    Create D04 NumPy 陣列邏輯函式 (Logic functions).py
    陣列邏輯函式 (Logic functions)

---
+++ D04 NumPy 陣列邏輯函式 (Logic functions).py
@@ -0,0 +1,15 @@
+import numpy as np
+# 題目:
+english_score = np.array([55,89,76,65,48,70])
+math_score = np.array([60,85,60,68,55,60])
+chinese_score = np.array([65,90,82,72,66,77])
+# 上3列共六位同學的英文、數學、國文成績，第一個元素代表第一位同學，舉例第一位同學英文55分、數學60分、國文65分，運用上列數據回答下列問題。
+
+# 1. 有多少學生英文成績比數學成績高?
+a = np.sum(np.greater(english_score, math_score))
+print(a)
+
+# 2. 是否全班同學最高分都是國文?
+b = np.logical_and(np.greater(chinese_score, math_score),np.greater(chinese_score, english_score))
+if np.all(b) == True:
+    print(True)
